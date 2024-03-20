fdf0e901ae6c7b46ba8d696015459751c401d6b5
Hirday26 <hirdayrochani@gmail.com>
2024-03-20 17:30:06 +0530

    added functionality of attendance

---
+++ SpringBoot/hrisportal/hrisportal/src/main/java/com/sgt/hrisportal/repository/AttendanceRepository.java
@@ -0,0 +1,21 @@
+package com.sgt.hrisportal.repository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class AttendanceRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> markAttendance() {
+        return jdbcTemplate.queryForMap("EXEC sp_insert_datetime");
+    }
+
+}
---
+++ SpringBoot/hrisportal/hrisportal/src/main/java/com/sgt/hrisportal/web/AttendanceResource.java
@@ -0,0 +1,25 @@
+package com.sgt.hrisportal.web;
+import com.sgt.hrisportal.service.AttendanceService;
+import com.sgt.hrisportal.service.userService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200",allowCredentials = "true")
+public class AttendanceResource {
+    @Autowired
+    AttendanceService attendanceService;
+
+    @PostMapping("/attendance")
+    public Map<String, Object> markAttendance() {
+        return attendanceService.markAttendance();
+    }
+    }
+
